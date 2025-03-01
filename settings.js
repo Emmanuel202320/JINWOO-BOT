76a342e75c1e4b9b6c5fe18775930c4f07c36253
Emmanuel202320 <emmanuelbillions12345@gmail.com>
2025-03-01 14:17:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,7 +14,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
 //=================================================//
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'JIN~k9B1CBgb#QMKsT3x4pt7oTQOH-F4ve-_6b6sjH-_z_cI7mkPVf8s' 
 //Enter your JINWOO BOT session id here; must start with JIN~
 
 //=================================================//
